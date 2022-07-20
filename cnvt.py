abb3501f52bf8b0438940fb24506bff010e65f38
rcph <rcph110110@gmail.com>
2022-07-20 21:05:23 +0800

    futeur_cke

---
+++ cnvt.py
@@ -0,0 +1,16 @@
+from moviepy.editor import *
+import os
+
+
+print("<------------- folderName <mp4 convert_to mp3> ------------->")
+folderName = str(input())
+print("<------------- folderName <export/where to save> ------------->")
+pathName = str(input())
+
+for i in os.listdir(folderName): 
+    path = pathName + "/" 
+    videoclip = VideoFileClip(folderName + '/' + i)
+    audiofile = videoclip.audio.write_audiofile(i[:-4]+".mp3")
+    if not os.path.exists(path):
+        os.mkdir(path)
+    os.rename(i[:-4]+".mp3",path+i[:-4]+".mp3")
