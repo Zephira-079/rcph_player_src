8a21581920d71e24652a5da75f0b220d75553e09
rcph <rcph110110@gmail.com>
2022-11-16 20:21:34 +0800

    new_upload

--- cnvt.py
+++ cnvt.py
@@ -1,16 +1,30 @@
 from moviepy.editor import *
 import os
 
-
-print("<------------- folderName <mp4 convert_to mp3> ------------->")
-folderName = str(input())
+print("<------------- folderName <mp4 convert_to mp3/find folder> ------------->")
+folderName = str(input()).strip()
 print("<------------- folderName <export/where to save> ------------->")
-pathName = str(input())
+pathName = str(input()).strip()
 
-for i in os.listdir(folderName): 
-    path = pathName + "/" 
-    videoclip = VideoFileClip(folderName + '/' + i)
-    audiofile = videoclip.audio.write_audiofile(i[:-4]+".mp3")
+for i in os.listdir(folderName):
+    path = f'{pathName}'
+    try:
+        videoclip = VideoFileClip(f'{folderName}/{i}')
+    except:
+        with open("console.log","a") as c:
+            c.writelines(f'cant find: {i[:-4]}.mp3\n')
+        print("seems cant find it!!!")
+        
     if not os.path.exists(path):
         os.mkdir(path)
-    os.rename(i[:-4]+".mp3",path+i[:-4]+".mp3")
+
+    try:
+        if os.path.isfile(f'{path}/{i[:-4]}.mp3'):
+            print(f'file exists at : {path}/{i[:-4]}.mp3')
+        else:
+            audiofile = videoclip.audio.write_audiofile(f'{i[:-4]}.mp3')
+            os.rename(f'{i[:-4]}.mp3', f'{path}/{i[:-4]}.mp3')
+    except:
+        with open("console.log","a") as c:
+            c.writelines(f'cant find: {i[:-4]}.mp3\n')
+        print("seems cant find it!!!")
